0f465ea9f5ef101d50efca5e42682ccd488f2b03
Scarface69420 <nixonkurian.nk@gmail.com>
2023-08-04 15:43:14 +0530

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿namespace UserSignUp.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+             
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=userdb;Integrated Security=True;");
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
